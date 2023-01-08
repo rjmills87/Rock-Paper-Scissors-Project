724e98ade865ff6a17b3ba1e2a07574573768fb4
Rhys Miller <rjmills87@aol.com>
2023-01-08 20:38:06 +0000

    Game logic works correctly with event handlers.

--- script.js
+++ script.js
@@ -1,61 +1,72 @@
-const array = ["Rock", "Paper", "Scissors"];
-
-//Initial player scores
-let playerScore = 0;
-let computerScore = 0;
-
 //Generate Random Player Choice
-function getComputerChoice(arr) {
+const getComputerChoice = (arr) => {
   const randomSelection = Math.floor(Math.random() * arr.length);
   const choice = arr[randomSelection];
   return choice;
-}
+};
 
-//Game logic
-function playRound(playerSelection, computerSelection) {
+//Initial values
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+let playerChoice = "";
+
+//Button selectors
+let rockBtn = document.querySelector(".rock-button");
+let paperBtn = document.querySelector(".paper-button");
+let scissorsBtn = document.querySelector(".scissors-button");
+let startBtn = document.querySelector(".start-button");
+let resetBtn = document.querySelector(".reset-button");
+
+//Event handlers
+rockBtn.addEventListener("click", () => {
+  playerChoice = "ROCK";
+  playRound();
+});
+
+paperBtn.addEventListener("click", () => {
+  playerChoice = "PAPER";
+  playRound();
+});
+
+scissorsBtn.addEventListener("click", () => {
+  playerChoice = "SCISSORS";
+  playRound();
+});
+
+const playRound = () => {
+  let computerSelection = getComputerChoice(choices);
+  let playerSelection = playerChoice;
   if (
     (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
     (playerSelection === "PAPER" && computerSelection === "ROCK") ||
     (playerSelection === "SCISSORS" && computerSelection === "PAPER")
   ) {
     playerScore++;
-    return `Well done! ${playerSelection} beats ${computerSelection}.`;
+    roundCount++;
+    console.log(`Well done! ${playerSelection} beats ${computerSelection}.`);
+    console.log(playerScore, computerScore);
   } else if (
     (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
     (computerSelection === "PAPER" && playerSelection === "ROCK") ||
     (computerSelection === "SCISSORS" && playerSelection === "PAPER")
   ) {
+    roundCount++;
     computerScore++;
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    console.log(playerScore, computerScore);
   } else if (playerSelection === computerSelection) {
-    return "It's a tie.";
-  } else {
-    alert("Wrong selection!!! Please choose ROCK 🪨, PAPER 📄 or SCISSORS ✂️.");
+    console.log("It's a tie.");
+    console.log(playerScore, computerScore);
   }
-}
+};
 
-//Playing through the game
-function game() {
-  for (i = 1; i < 30; ++i) {
-    if (playerScore === 5 && computerScore < 5) {
-      console.log("Well done. You are the Winner!!! To play again press OK");
-      window.location.reload(true);
-    } else if (playerScore < 5 && computerScore === 5) {
-      console.log("You lose. Better luck next time. To play again press OK");
-      window.location.reload(true);
-    } else {
-      const playerChoice = prompt(
-        "What do you choose: Rock, Paper or Scissors"
-      );
-      const computerChoice = getComputerChoice(array);
-      const playerSelection = playerChoice.toUpperCase();
-      const computerSelection = computerChoice.toUpperCase();
-      console.log(
-        `You chose ${playerChoice.toUpperCase()} and the CPU chose ${computerChoice.toUpperCase()}`
-      );
-      console.log(playRound(playerSelection, computerSelection));
-      console.log(playerScore, computerScore);
-    }
-  }
-}
-game();
+startBtn.addEventListener("click", () => {
+  game();
+});
+
+resetBtn.addEventListener("click", () => {
+  roundCount = 0;
+});
